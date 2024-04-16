39e8376d2ec3a1251bcf0ad21263c3c5adb52619
sk <sk@bitsandlikes.de>
2024-04-16 23:18:45 +0200

    day 15: part two

--- day15/Day15.cs
+++ day15/Day15.cs
@@ -27,7 +27,136 @@ public class Day15
 
     public static long PartTwoSolution(StreamReader sr)
     {
-        return 0;
+        int c = 0;
+        string label = "";
+        int hash = 0;
+        Dictionary<int, Box> boxes = new Dictionary<int, Box>();
+
+        while ((c = sr.Read()) > -1)
+        {
+            if (c == (int)'=' || c == (int)'-')
+            {
+                switch (c)
+                {
+                    case (int)'=':
+                        if (!boxes.ContainsKey(hash))
+                        {
+                            Box box = new Box();
+                            int focalLength = sr.Read() - 48;
+                            Lense lense = new Lense(label, focalLength);
+                            box.AddLense(lense);
+                            boxes.Add(hash, box);
+                        }
+                        else
+                        {
+                            int focalLength = sr.Read() - 48;
+                            Lense lense = new Lense(label, focalLength);
+                            boxes[hash].AddLense(lense);
+                        }
+                        sr.Read();
+                        hash = 0;
+                        label = "";
+                        continue;
+
+                    case (int)'-':
+                        if (boxes.ContainsKey(hash))
+                        {
+                            boxes[hash].RemoveLense(label);
+                        }
+                        sr.Read();
+                        hash = 0;
+                        label = "";
+                        continue;
+                }
+            }
+            label += (char)c;
+            hash += c;
+            hash = (hash * 17) % 256;
+        }
+        int result = 0;
+        foreach (KeyValuePair<int, Box> kvp in boxes)
+        {
+            Lense[] lenses = kvp.Value.GetLenses().ToArray();
+            for (int i = 0; i < lenses.Length; i++)
+            {
+                result += (kvp.Key + 1) * (i + 1) * lenses[i].GetFocalLength();
+            }
+        }
+        return result;
+    }
+
+    private class Box
+    {
+        private List<Lense> _lenses;
+
+        public Box()
+        {
+            _lenses = new List<Lense>();
+        }
+
+        public void RemoveLense(string label)
+        {
+            int idx = _lenses.FindIndex(x => x.GetLabel() == label);
+            if (idx > -1)
+            {
+                _lenses.RemoveAt(idx);
+            }
+        }
+
+        public void AddLense(Lense lense)
+        {
+            int idx = _lenses.FindIndex(x => x.GetLabel() == lense.GetLabel());
+            if (idx > -1)
+            {
+                _lenses[idx] = lense;
+            }
+            else
+            {
+                _lenses.Add(lense);
+            }
+        }
+
+        public List<Lense> GetLenses()
+        {
+            return _lenses;
+        }
+
+        public override string ToString()
+        {
+            string s = "";
+            foreach (Lense lense in _lenses)
+            {
+                s += $"[{lense}]";
+            }
+            return s;
+        }
+    }
+
+    private class Lense
+    {
+        private string _label;
+        private int _focalLength;
+
+        public Lense(string label, int focalLength)
+        {
+            _label = label;
+            _focalLength = focalLength;
+        }
+
+        public override string ToString()
+        {
+            return _label + _focalLength;
+        }
+
+        public int GetFocalLength()
+        {
+            return _focalLength;
+        }
+
+        public string GetLabel()
+        {
+            return _label;
+        }
     }
 
     public static void PartOneTest()
@@ -73,9 +202,9 @@ class Program
 {
     static void Main()
     {
-        // Day15.PartOneTest();
+        Day15.PartOneTest();
         Day15.PartOne();
-        // Day15.PartTwoTest();
-        // Day15.PartTwo();
+        Day15.PartTwoTest();
+        Day15.PartTwo();
     }
 }
